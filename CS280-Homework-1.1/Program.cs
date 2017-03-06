7a71040e559c2aee211375068ab85984dccdf611
wood0706 <wood0706@gmail.com>
2017-03-06 14:40:42 +0800

    初始化專案

---
+++ CS280-Homework-1.1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS280_Homework_1._1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
